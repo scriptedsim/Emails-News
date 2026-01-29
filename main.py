0c22e604e76d520d7e8d8c177df53db7579b7d39
scriptedsim <simeenayman22@gmail.com>
2026-01-29 10:16:38 +0530

    Five implementations

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests
 from send_email import send_email
 
 api_key = "39f9e10242914fdf97d596010557366d"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2025-12-28&sortBy=publishedAt&apiKey=39f9e10242914fdf97d596010557366d"
+topic = "tesla"
+
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2026-01-28&sortBy=publishedAt&apiKey={api_key}&language=en"
 
 # Make a request
 request = requests.get(url)
@@ -11,9 +13,9 @@ request = requests.get(url)
 content = request.json()
 
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None and article["description"] is not None:
-        body += f"{article['title']}\n{article['description']}\n\n"
+        body = body + f"Subject: Today's News\n{article['title']}\n{article['description']}\n{article['url']}\n\n"
 
 body = body.encode('utf-8')
 send_email(message=body)
